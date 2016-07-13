f29a001cc63b03c91a31e753e693d8fce722beeb
Adam Chalmers <adam.s.chalmers@gmail.com>
2016-07-13 12:23:33 +1000

    St. Petersburg calculator.

---
+++ petersburg.py
@@ -0,0 +1,22 @@
+import sys
+
+ERR = "Please enter a confidence, as a decimal number."
+
+def petersburg(v):
+  flips = 0
+  p = 1
+  while p > v:
+    flips += 1
+    p *= 0.5
+  pay = 2**flips
+  return "There's only a %s chance that you'll win more than $%d" % (p, pay)
+
+if __name__ == "__main__":
+  if len(sys.argv) < 2 or "h" in sys.argv[1]:
+    print "Argument missing. %s" % ERR
+  else:
+    try:
+      v = float(sys.argv[1])
+      print(petersburg(v))
+    except ValueError:
+      print("'%s' isn't a valid decimal number. %s" % (sys.argv[1], ERR))
